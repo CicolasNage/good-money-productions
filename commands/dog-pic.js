ea72cabb748339ee42a7f19235bc5fba9454b3b2
Cicolas Nage <brboles87@gmail.com>
2020-03-28 00:11:15 -0600

    Trying full example from discordjs.guide for dog-pic.

--- commands/dog-pic.js
+++ commands/dog-pic.js
@@ -14,13 +14,57 @@ module.exports = {
   
   execute (message, args) {
     const dogLink = fetchDogLink();
-    const exampleEmbed = new Discord.MessageEmbed()
-	.setTitle('Some title')
-	.attachFiles([dogLink])
-	.setImage('attachment://discordjs.png');
-
-channel.send(exampleEmbed);
-
+    const exampleEmbed = {
+        color: 0x0099ff,
+        title: 'Some title',
+        url: 'https://discord.js.org',
+        author: {
+            name: 'Some name',
+            icon_url: 'https://i.imgur.com/wSTFkRM.png',
+            url: 'https://discord.js.org',
+        },
+        description: 'Some description here',
+        thumbnail: {
+            url: 'https://i.imgur.com/wSTFkRM.png',
+        },
+        fields: [
+            {
+                name: 'Regular field title',
+                value: 'Some value here',
+            },
+            {
+                name: '\u200b',
+                value: '\u200b',
+                inline: false,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+        ],
+        image: {
+            url: dogLink,
+        },
+        timestamp: new Date(),
+        footer: {
+            text: 'Some footer text here',
+            icon_url: 'https://i.imgur.com/wSTFkRM.png',
+        },
+    };
+    
+    channel.send({ embed: exampleEmbed });
+    
 
 
 
