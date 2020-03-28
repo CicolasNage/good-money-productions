1f61bd4f998330926bb41b86e02f2b31e009ced8
Cicolas Nage <brboles87@gmail.com>
2020-03-27 23:52:27 -0600

    tts?

--- commands/onions.js
+++ commands/onions.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'onions',
     description: 'All about silvered onions',
     execute(message, args) {
-      message.channel.tts().send("No, they're called silvered onions.  Because they're silver.");
+      message.channel.tts("No, they're called silvered onions.  Because they're silver.");
     },
   };
