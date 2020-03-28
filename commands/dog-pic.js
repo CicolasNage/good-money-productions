57e4a5bd602aa78264da1494c2806dcbf39359ba
Cicolas Nage <brboles87@gmail.com>
2020-03-28 00:32:33 -0600

    will farting work?

--- commands/dog-pic.js
+++ commands/dog-pic.js
@@ -11,56 +11,16 @@ module.exports = {
   name: 'dog-pic',
   description: 'Returns a random picture of a pup from the dog api',
   
-  
+
   execute (message, args) {
-    
+    const dog = fetchDogLink();
     const exampleEmbed = {
         color: 0x0099ff,
         title: 'Some title',
         url: 'https://discord.js.org',
-        author: {
-            name: 'Some name',
-            icon_url: 'https://i.imgur.com/wSTFkRM.png',
-            url: 'https://discord.js.org',
-        },
-        description: 'Some description here',
-        thumbnail: {
-            url: 'https://i.imgur.com/wSTFkRM.png',
-        },
-        fields: [
-            {
-                name: 'Regular field title',
-                value: 'Some value here',
-            },
-            {
-                name: '\u200b',
-                value: '\u200b',
-                inline: false,
-            },
-            {
-                name: 'Inline field title',
-                value: 'Some value here',
-                inline: true,
-            },
-            {
-                name: 'Inline field title',
-                value: 'Some value here',
-                inline: true,
-            },
-            {
-                name: 'Inline field title',
-                value: 'Some value here',
-                inline: true,
-            },
-        ],
-        timestamp: new Date(),
-        footer: {
-            text: 'Some footer text here',
-            icon_url: 'https://i.imgur.com/wSTFkRM.png',
-        },
     };
     
-    message.channel.send(fetchDogLink());
+    message.channel.send(dog);
     
 
 
