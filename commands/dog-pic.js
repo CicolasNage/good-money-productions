02c6bfc0226991b5d86935c6a0ed29c049de4248
Cicolas Nage <brboles87@gmail.com>
2020-03-28 00:39:25 -0600

    will farting work?

--- commands/dog-pic.js
+++ commands/dog-pic.js
@@ -3,11 +3,12 @@ const Discord = require('discord.js');
 const Canvas = require('canvas')
 
 fetchDogLink = () => {
-    let dogLink = fetch('https://dog.ceo/api/breeds/image/random').then(res=>res.json().message)
-    return 'dogLink will be here if it is working';
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(res => res)
+    
 }
 
-const dog = fetchDogLink();
 module.exports = {
   name: 'dog-pic',
   description: 'Returns a random picture of a pup from the dog api',
@@ -15,11 +16,7 @@ module.exports = {
 
   execute (message, args) {
     
-    const exampleEmbed = {
-        color: 0x0099ff,
-        title: 'Some title',
-        url: 'https://discord.js.org',
-    };
+    let dog = fetchDogLink();
     
     message.channel.send(dog);
 
