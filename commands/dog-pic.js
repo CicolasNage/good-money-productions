4d9ad9735ff65467652bb7a62e24a70eaa915ee6
Cicolas Nage <brboles87@gmail.com>
2020-03-28 00:03:54 -0600

    tts?

---
+++ commands/dog-pic.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const Discord = require('discord.js');
+const Canvas = require('canvas')
+
+fetchDogLink = () => {
+    let dogLink = fetch('https://dog.ceo/api/breeds/image/random').then(res=>res.message)
+    return dogLink;
+}
+
+module.exports = {
+  name: 'dog-pic',
+  description: 'Returns a random picture of a pup from the dog api',
+  
+  
+  execute (message, args) {
+    const dogLink = fetchDogLink();
+    const exampleEmbed = new Discord.MessageEmbed()
+	.setTitle('Some title')
+	.attachFiles([dogLink])
+	.setImage('attachment://discordjs.png');
+
+channel.send(exampleEmbed);
+
+
+
+
+  },
+};
