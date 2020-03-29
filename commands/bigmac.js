c9181adfc4e80214e05afbfab8fca83feb8ae02d
Cicolas Nage <brboles87@gmail.com>
2020-03-28 23:06:02 -0600

    Ughmmmughmmghmgh

--- commands/bigmac.js
+++ commands/bigmac.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'bigmac',
     description: 'If I had a Big Mac...',
     execute(message, args) {
-      message.channel.send("If I had a Bic Mac I'd eat it.  ugh mmm ugh mmm", {tts: true});
+      message.channel.send("If I had a Bic Mac I'd eat it.  ughmughmmm", {tts: true});
     },
   };
