9f2054881e4d67a9eca4900c39fca02e14e593ca
Blade <bladerboles@gmail.com>
2021-02-13 13:44:43 -0700

    nope

--- commands/nope.js
+++ commands/nope.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'nope',
-  description: 'beep aye',
+  name: "nope",
+  description: "nope",
   execute(message, args) {
-    message.channel.send('N\nO\nP\nE');
+    message.channel.send("N\nO\nP\nE");
   },
 };
